6edfb1e2a5fd9d715c9b60e531b2473e876659f0
Guilherme Bertoldo <glbertoldo@gmail.com>
2017-10-26 11:47:06 -0200

    Reorganized main algorithm.

--- depp_src/depp_class_optimizer.f90
+++ depp_src/depp_class_optimizer.f90
@@ -136,13 +136,14 @@ contains
       integer                            :: iaux    ! Dummy variable
       integer                            :: ind     ! Number of the individual
       integer                            :: estatus ! Exit status (0=success; 1=failure)
-      real(8)                            :: xfit    ! Fitness of the trial individual
-      real(8), dimension(:), allocatable :: x       ! Trial individual
+      real(8), allocatable               :: xfit(:)    ! Fitness of the trial individual
+      real(8), dimension(:,:), allocatable :: x       ! Trial individual
 
 
       ! To be removed...
       real(8) :: fh =2       !< Fraction of hybridization
-      integer :: rsm_tag   !< Stores the return state of application of DE-RSM
+      integer, allocatable :: rsm_tag(:)   !< Stores the return state of application of DE-RSM
+      integer :: to_thread, from_thread
 
 
       ! Creating labels
@@ -155,7 +156,9 @@ contains
 
 
       ! Allocating resources
-      allocate(x(ehist%nu))
+      allocate(x(ehist%np,ehist%nu))
+      allocate(xfit(ehist%np))
+      allocate(rsm_tag(ehist%np))
 
 
       !
@@ -211,16 +214,16 @@ contains
                if ( ehist%g == 1 ) then
 
                   ! rsm_tag stores the return state of application of DE-RSM
-                  rsm_tag = DE_RSM_RETURN%DE_APPLIED
+                  rsm_tag(ind) = DE_RSM_RETURN%DE_APPLIED
 
                   ! Calculating the fitness function
                   fitloop1: do
 
                      ! Creating the trial individual x
-                     call get_random_individual(ehist%nu, ehist%xmin, ehist%xmax, x)
+                     call get_random_individual(ehist%nu, ehist%xmin, ehist%xmax, x(ind,:))
 
-                     call get_fitness(sys_var, ehist%sname, ind, ehist%nu, x, ehist%xname, &
-                        xfit, estatus)
+                     call get_fitness(sys_var, ehist%sname, ind, ehist%nu, x(ind,:), ehist%xname, &
+                        xfit(ind), estatus)
 
                      ! Analyzing the exit status of the external program
                      select case (estatus)
@@ -233,7 +236,7 @@ contains
 
                            ! Failure in the calculation of fitness function. Saving informations.
                            call save_fitness_failure(ehist%nu, ehist%g, ind, sys_var, ehist%sname, &
-                              x, estatus)
+                              x(ind,:), estatus)
 
                         case default
 
@@ -256,42 +259,42 @@ contains
                      if ( rsm_check(ehist%np, ehist%g, fh) ) then
 
                         ! rsm_tag stores the return state of application of DE-RSM
-                        rsm_tag = DE_RSM_RETURN%RSM_APPLIED
+                        rsm_tag(ind) = DE_RSM_RETURN%RSM_APPLIED
 
                         ! Generating a RSM individual
 
                         call get_rsm_optimum(ind, ehist%nu, ehist%np, ehist%ng, ehist%g, ehist%xmin,&
-                         ehist%xmax, ehist%pop, ehist%hist, x, estatus)
+                         ehist%xmax, ehist%pop, ehist%hist, x(ind,:), estatus)
 
 
                         ! If RSM fails, generates a pure DE individual
                         if ( estatus == 1 ) then
 
                            ! rsm_tag stores the return state of application of DE-RSM
-                           rsm_tag = DE_RSM_RETURN%DE_APPLIED_AFTER_RSM_FAILURE
+                           rsm_tag(ind) = DE_RSM_RETURN%DE_APPLIED_AFTER_RSM_FAILURE
 
                            ! Creating the trial individual x
-                           call searcher%get_trial(ind, ehist%pop, x)
+                           call searcher%get_trial(ind, ehist%pop, x(ind,:))
 
                         end if
 
                      else
 
                         ! rsm_tag stores the return state of application of DE-RSM
-                        rsm_tag = DE_RSM_RETURN%DE_APPLIED
+                        rsm_tag(ind) = DE_RSM_RETURN%DE_APPLIED
 
                         ! Creating the trial individual x
-                          call searcher%get_trial(ind, ehist%pop, x)
+                          call searcher%get_trial(ind, ehist%pop, x(ind,:))
 
                      end if
 
 
                      ! Verifying the constraints. If the individual x is out of range,
                      ! another one is created using pure DE
-                     do while ( is_X_out_of_range(ehist%nu, ehist%xmin, ehist%xmax, x) )
+                     do while ( is_X_out_of_range(ehist%nu, ehist%xmin, ehist%xmax, x(ind,:)) )
 
                         ! Checking DE-RSM status
-                        select case (rsm_tag)
+                        select case (rsm_tag(ind))
 
                            ! If DE was applied, do nothing.
                            case (DE_RSM_RETURN%DE_APPLIED)
@@ -299,7 +302,7 @@ contains
                            ! If RSM was applied, a DE individual will be generated. Counts this application as a RSM failure.
                            case (DE_RSM_RETURN%RSM_APPLIED)
 
-                              rsm_tag = DE_RSM_RETURN%DE_APPLIED_AFTER_RSM_FAILURE
+                              rsm_tag(ind) = DE_RSM_RETURN%DE_APPLIED_AFTER_RSM_FAILURE
 
                            ! If DE was applied after a RSM failure, counts this application as a RSM failure.
                            case (DE_RSM_RETURN%DE_APPLIED_AFTER_RSM_FAILURE)
@@ -313,14 +316,14 @@ contains
 
 
                         ! Creating the trial individual x
-                        call searcher%get_trial(ind, ehist%pop, x)
+                        call searcher%get_trial(ind, ehist%pop, x(ind,:))
 
                      end do
 
 
                      ! Asking to the external program 'ffit' the fitness of individual 'x'
-                     call get_fitness(sys_var, ehist%sname, ind, ehist%nu, x, ehist%xname, &
-                        xfit, estatus)
+                     call get_fitness(sys_var, ehist%sname, ind, ehist%nu, x(ind,:), ehist%xname, &
+                        xfit(ind), estatus)
 
                      ! Analyzing the exit status of the external program
                      select case (estatus)
@@ -332,26 +335,26 @@ contains
                         case (1) ! Failure
 
                            ! rsm_tag stores the return state of application of DE-RSM
-                           rsm_tag = DE_RSM_RETURN%BLACK_BOX_EVALUATION_FAILURE
+                           rsm_tag(ind) = DE_RSM_RETURN%BLACK_BOX_EVALUATION_FAILURE
 
                            ! Failure in the calculation of fitness function. Saving informations.
                            call save_fitness_failure(ehist%nu, ehist%g, ind, sys_var, ehist%sname, &
-                              x, estatus)
+                              x(ind,:), estatus)
 
-                           x = ehist%pop(ind,:)
+                           x(ind,:) = ehist%pop(ind,:)
 
-                           xfit = ehist%fit(ind)
+                           xfit(ind) = ehist%fit(ind)
 
                            exit fitloop2
 
                         case (2:10) ! Generate another individual
 
                            ! rsm_tag stores the return state of application of DE-RSM
-                           rsm_tag = DE_RSM_RETURN%BLACK_BOX_EVALUATION_FAILURE
+                           rsm_tag(ind) = DE_RSM_RETURN%BLACK_BOX_EVALUATION_FAILURE
 
                            ! Failure in the calculation of fitness function. Saving informations.
                            call save_fitness_failure(ehist%nu, ehist%g, ind, sys_var, ehist%sname, &
-                              x, estatus)
+                              x(ind,:), estatus)
 
                         case default
 
@@ -365,17 +368,73 @@ contains
 
                end if
 
-               ! Sending informations to master processor
-               call mpi_send(    ind,  1,          mpi_integer, 0, mpi%tag, mpi%comm, mpi%code)
-               call mpi_send(      x, ehist%nu, mpi_double_precision, 0, mpi%tag, mpi%comm, mpi%code)
-               call mpi_send(   xfit,  1, mpi_double_precision, 0, mpi%tag, mpi%comm, mpi%code)
-               call mpi_send(rsm_tag,  1,          mpi_integer, 0, mpi%tag, mpi%comm, mpi%code)
 
             end if
 
          end do
 
          call mod_mpi_barrier()
+
+         do i = 1, ehist%np
+
+            ! Data of index 'i' was calculated by thread_map(i)
+            from_thread = mod(i, mpi%nproc-1) + 1
+
+            ! If the current thread is the same thread that calculated data i, send data to others thread
+            if ( mpi%iproc == from_thread ) then
+
+               do to_thread = 0, mpi%nproc-1
+
+                  if (to_thread/=from_thread) then
+
+                     call mpi_send(    x(i,:), ehist%nu, mpi_double_precision, to_thread, mpi%tag, mpi%comm, mpi%code)
+                     call mpi_send(   xfit(i),  1, mpi_double_precision, to_thread, mpi%tag, mpi%comm, mpi%code)
+                     call mpi_send(rsm_tag(i),  1,          mpi_integer, to_thread, mpi%tag, mpi%comm, mpi%code)
+
+                  end if
+
+               end do
+
+            ! Otherwise, receive data
+            else
+
+               call mpi_recv(    x(i,:), ehist%nu, mpi_double_precision, from_thread, mpi%tag, mpi%comm, mpi%status, mpi%code)
+               call mpi_recv(   xfit(i),  1, mpi_double_precision, from_thread, mpi%tag, mpi%comm, mpi%status, mpi%code)
+               call mpi_recv(rsm_tag(i),  1,          mpi_integer, from_thread, mpi%tag, mpi%comm, mpi%status, mpi%code)
+
+            end if
+
+         end do
+
+         call mod_mpi_barrier()
+
+         ! For each individual of the population
+         do i = 1, ehist%np
+
+            ! Updating history
+            ehist%hist(ehist%g,i,1:ehist%nu) = x(i,:)    ! Individual
+            ehist%hist(ehist%g,i,0)    = xfit(i) ! Fitness of the individual
+
+
+            ! Updating RSM Dynamic Control module
+            call add_to_rsm_dynamic_control(rsm_tag(i), xfit(i), ehist%fit(i))
+
+            ! Selecting the best individual
+            if ( xfit(i) >= ehist%fit(i)) then
+
+               ehist%pop(i,:) = x(i,:)
+
+               ehist%fit(i) = xfit(i)
+
+               if ( xfit(i) >= ehist%fit(ehist%ibest)) ehist%ibest = i
+
+            end if
+
+         end do
+
+         ! Calculating the hybridization factor
+         fh = get_hybridization_factor()
+
 
 
          ! If iproc == 0, master processor receives the information from slave processors,
@@ -386,52 +445,13 @@ contains
             ! For each individual of the population
             do i = 1, ehist%np
 
-               ! Selecting the processor
-               iaux = mod(i, mpi%nproc-1) + 1
-
-               ! Recieving informations from slaves
-               call mpi_recv(    ind,  1,          mpi_integer, iaux, mpi%tag, mpi%comm, mpi%status, mpi%code)
-               call mpi_recv(      x, ehist%nu, mpi_double_precision, iaux, mpi%tag, mpi%comm, mpi%status, mpi%code)
-               call mpi_recv(   xfit,  1, mpi_double_precision, iaux, mpi%tag, mpi%comm, mpi%status, mpi%code)
-               call mpi_recv(rsm_tag,  1,          mpi_integer, iaux, mpi%tag, mpi%comm, mpi%status, mpi%code)
-
-               ! Updating history
-               ehist%hist(ehist%g,ind,1:ehist%nu) = x    ! Individual
-               ehist%hist(ehist%g,ind,0)    = xfit ! Fitness of the individual
-
-
-               ! Updating RSM Dynamic Control module
-               call add_to_rsm_dynamic_control(rsm_tag, xfit, ehist%fit(ind))
-
-
                write(*,"(a, i4, a, 10(1pe23.15, 2x))") &
-                  "The performance of the",  ind, "th individual is ", xfit, x
+                  "The performance of the",  i, "th individual is ", xfit(i), x(i,:)
 
-               write(21,"(2(i12), 100(2x, 1pe23.15))") ehist%g, ind, xfit, x
+               write(21,"(2(i12), 100(2x, 1pe23.15))") ehist%g, i, xfit(i), x(i,:)
 
                call flush(21)
 
-               ! Selecting the best individual
-               if ( xfit >= ehist%fit(ind)) then
-
-                  ehist%pop(ind,:) = x
-
-                  ehist%fit(ind) = xfit
-
-                  if ( xfit >= ehist%fit(ehist%ibest)) ehist%ibest = ind
-
-               end if
-
-            end do
-
-
-            ! Calculating the hybridization factor
-            fh = get_hybridization_factor()
-
-
-            ! Sending convergence measure to slave processors
-            do i = 1, mpi%nproc-1
-               call mpi_send(fh,    1, mpi_double_precision, i, mpi%tag, mpi%comm, mpi%code)
             end do
 
             write(20,"(i12, 3(2x, 1pe23.15),A)") ehist%g, sum(ehist%fit)/ehist%np, maxval(ehist%fit), fh
@@ -443,39 +463,6 @@ contains
 
             ! Saving backup data
             call ehist%save_backup(sys_var)
-
-         end if
-
-         ! If iproc /= 0, slave processors receive the convergence measure from the master processor
-         if ( mpi%iproc > 0 ) then
-            call mpi_recv(fh,    1, mpi_double_precision, 0, mpi%tag, mpi%comm, mpi%status, mpi%code)
-         end if
-
-         call mod_mpi_barrier()
-
-         ! If iproc == 0, master processor sends the updated population and its fitness to
-         !                slave processors
-         ! If iproc /= 0, slave processors receive the updated population and its fitness
-         if (mpi%iproc == 0) then
-
-            ! Sending the population and its fitness to slave processors
-            do i = 1, mpi%nproc-1
-               call mpi_send(ehist%hist, ehist%ng*ehist%np*(ehist%nu+1), mpi_double_precision, i&
-               , mpi%tag, mpi%comm, mpi%code)
-               call mpi_send(ehist%pop,         ehist%np*ehist%nu, mpi_double_precision, i &
-               , mpi%tag, mpi%comm, mpi%code)
-               call mpi_send(ehist%fit,            ehist%np, mpi_double_precision, i, mpi%tag, mpi%comm, mpi%code)
-            end do
-
-         else
-
-            ! Receiving the population and its fitness from master processor
-            call mpi_recv(ehist%hist, ehist%ng*ehist%np*(ehist%nu+1), mpi_double_precision, 0&
-            , mpi%tag, mpi%comm, mpi%status, mpi%code)
-            call mpi_recv(ehist%pop,         ehist%np*ehist%nu, mpi_double_precision, 0&
-            , mpi%tag, mpi%comm, mpi%status, mpi%code)
-            call mpi_recv(ehist%fit,            ehist%np, mpi_double_precision, 0&
-            , mpi%tag, mpi%comm, mpi%status, mpi%code)
 
          end if
 
